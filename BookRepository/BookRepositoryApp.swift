a46946fc3612af56e05b2fb1b3b6c50cd67d16fe
Aruna Randika <randikaroxxs98@gmail.com>
2023-03-21 16:37:30 +0530

    Initial Commit

---
+++ BookRepository/BookRepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookRepositoryApp.swift
+//  BookRepository
+//
+//  Created by Aruna Randika on 2023-03-21.
+//
+
+import SwiftUI
+
+@main
+struct BookRepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
